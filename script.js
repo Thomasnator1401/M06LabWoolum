ffa530b6de2ce02f7286b3bd99ec5f2dd0f3c7ec
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-10 21:10:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-$(document).ready(function() {
-  $('#contactForm').submit(function(e) {
-      e.preventDefault();
-      alert('Thank you for reaching out! We will get back to you soon.');
-  });
-});
+// A function to check if a string is a palindrome
+function isPalindrome(str) {
+  // Convert the string to lowercase and remove any non-alphanumeric characters
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  // Initialize a variable to store the reversed string
+  let reversed = "";
+  // Loop through the string from the end and append each character to the reversed string
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  // Compare the original and the reversed strings and return true if they are equal, false otherwise
+  return str === reversed;
+}
+// A function to check if a string is a palindrome
+function isPalindrome(str) {
+  // Convert the string to lowercase and remove any non-alphanumeric characters
+}
